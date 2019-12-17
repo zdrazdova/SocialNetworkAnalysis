81c41f912426473052d70712dcb56662e308f7d2
Zuzana Drazdova <zdrazdova@gmail.com>
2019-12-17 23:06:10 +0100

    Vážený graf spojů mezi hlavními letišti

--- analyze.py
+++ analyze.py
@@ -24,37 +24,17 @@ for r in range(len(remove)):
     node = remove[r][1]
     nodes_to_remove.append(node)
     Gselection.remove_node(node)
-    
-
-#nx.draw_circular(Gselection,pos=nx.spring_layout(Gselection))
-weights_array = []
-for edge in Gselection.edges:
-    weight = Gselection.edges[edge]
-    weights_array.append(weight['weight']*100)
-
-print(weights_array)
-print(type(Gselection))
-print(type(G))
-print(type(G1))
-
-print(Gselection.edges)
-
-e2 = [(u, v, d) for (u, v, d) in Gselection.edges(data=True) if d['weight'] > 0.1 and d['weight'] < 0.2]
-e3 = [(u, v, d) for (u, v, d) in Gselection.edges(data=True) if d['weight'] <= 0.1 and d['weight'] > 0.05]
-e1 = [(u, v, d) for (u, v, d) in Gselection.edges(data=True) if d['weight'] > 0.2]
-e4 = [(u, v, d) for (u, v, d) in Gselection.edges(data=True) if d['weight'] <= 0.05]
 
 pos = nx.circular_layout(Gselection)
 
-nx.draw_networkx_nodes(Gselection, pos, node_size=100, with_labels=True)
+nx.draw_networkx(Gselection, pos, with_labels=True, node_size=100, edge_color="#ff000000")
+
+for (u, v, d) in Gselection.edges(data=True):
+    w =  d['weight']
+    edge = [(u,v,d)]
+    nx.draw_networkx_edges(Gselection, pos, edgelist=edge, equidistant=True, alpha=0.5, width=w*10)
 
 
-nx.draw_networkx_edges(Gselection, pos, edgelist=e1, equidistant=True, alpha=0.5, width=3.6)
-nx.draw_networkx_edges(Gselection, pos, edgelist=e2, equidistant=True, alpha=0.5, width=3)
-nx.draw_networkx_edges(Gselection, pos, edgelist=e3, equidistant=True, alpha=0.5, width=2.4)
-nx.draw_networkx_edges(Gselection, pos, edgelist=e4, equidistant=True, alpha=0.5, width=1.8)
-
-print(e1)
 
 
 
