94083f758de15a230f8541cc0275394a22c83598
Zuzana Drazdova <zdrazdova@gmail.com>
2019-12-18 23:29:26 +0100

    Slidy a mapa

--- analyze.py
+++ analyze.py
@@ -6,36 +6,67 @@ G=nx.read_pajek("USAir97.net")
 
 G1=nx.Graph(G)
 
-print("US Air 97 Network has {0} nodes and {1} edges.".format(len(G.nodes),len(G.edges)))
+with open("slides.md", "w") as file:
+    file.write("% Analysis of USAir97 network \n")
+    file.write("% Zuzana Drázdová & Zuzana Šimečková \n")
+    file.write("% January 7 2020 \n")
+    file.write("# General information \n")
+    file.write("US Air 97 Network has **{0}** nodes and **{1}** edges.\n".format(len(G.nodes),len(G.edges)))
+    file.write("Nodes represent Airports in the United States and edges represent routes between these airtports. Each edge has weights with indicated how many flights are on given route")
 
 
 
-sorted_d = sorted([(value, key) for (key,value) in G1.degree], reverse=True)
+    sorted_d = sorted([(value, key) for (key,value) in G1.degree], reverse=True)
 
-print("Top ten airports with most flights: ",sorted_d[:10])
-print(G.nodes['San Francisco Intl'])
-print(G.edges['Minot Intl', 'Stapleton Intl',0])
+    file.write("\n")
+    file.write("# Top US airports \n")
+    
+    file.write("**Top ten airports with most flights:** \n\n")
+    for i in range(10):
+        file.write("* " + str(sorted_d[i][1]) + "\n\n")
+        #file.write(sorted_d[:10])
+    Gselection = G
+    
+    #print(G.nodes['San Francisco Intl'])
 
-Gselection = G
+    remove = sorted_d[10:]
+    nodes_to_remove = []
+    for r in range(len(remove)):
+        node = remove[r][1]
+        nodes_to_remove.append(node)
+        Gselection.remove_node(node)
 
-remove = sorted_d[10:]
-nodes_to_remove = []
-for r in range(len(remove)):
-    node = remove[r][1]
-    nodes_to_remove.append(node)
-    Gselection.remove_node(node)
-
-pos = nx.circular_layout(Gselection)
-
-nx.draw_networkx(Gselection, pos, with_labels=True, node_size=100, edge_color="#ff000000")
-
-for (u, v, d) in Gselection.edges(data=True):
-    w =  d['weight']
-    edge = [(u,v,d)]
-    nx.draw_networkx_edges(Gselection, pos, edgelist=edge, equidistant=True, alpha=0.5, width=w*10)
+    pos = nx.circular_layout(Gselection)
 
 
 
+    #print(Gselection.nodes['San Francisco Intl'])
+    
+    positions = {}
+    for node in Gselection.nodes:
+        print(Gselection.nodes[node])
+        data = Gselection.nodes[node]
+        posi = (data['x'],-data['y'])
+        positions.setdefault(node, posi)
+    
+    print(positions)
+    
+    nx.draw_networkx(Gselection, pos=positions, with_labels=True, node_size=100, edge_color="#ff000000")
+    
+    for (u, v, d) in Gselection.edges(data=True):
+        w =  d['weight']
+        edge = [(u,v,d)]
+        nx.draw_networkx_edges(Gselection, pos=positions, edgelist=edge, equidistant=True, alpha=0.5, width=w*10)
+    
+    plt.savefig('most_flights.svg')
+    file.write("\n")
+    file.write("# Top US airports \n")
+    file.write("![](most_flights.png)")
+    
+    
+    
+    
+
+#print(G.edges['Minot Intl', 'Stapleton Intl',0])
 
 
-plt.show()
---
+++ slides.md
@@ -0,0 +1,32 @@
+% Analysis of USAir97 network 
+% Zuzana Drázdová & Zuzana Šimečková 
+% January 7 2020 
+# General information 
+US Air 97 Network has **332** nodes and **2126** edges.
+Nodes represent Airports in the United States and edges represent routes between these airtports. Each edge has weights with indicated how many flights are on given route
+# Top US airports 
+**Top ten airports with most flights:** 
+
+* Chicago O'hare Intl
+
+* Dallas/Fort Worth Intl
+
+* The William B Hartsfield Atlan
+
+* Pittsburgh Intll
+
+* Lambert-St Louis Intl
+
+* Charlotte/Douglas Intl
+
+* Stapleton Intl
+
+* Minneapolis-St Paul Intl/Wold-
+
+* Detroit Metropolitan Wayne Cou
+
+* San Francisco Intl
+
+
+# Top US airports 
+![](most_flights.png)
