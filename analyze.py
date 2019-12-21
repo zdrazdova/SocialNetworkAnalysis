b9b6892e1792bab9498ee780f23638d2da209a37
Zuzana Drazdova <zdrazdova@gmail.com>
2019-12-21 15:35:07 +0100

    Centralities added

--- analyze.py
+++ analyze.py
@@ -1,6 +1,24 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import copy as cp
+import numpy as np
 
+
+def compute_centrality(name, computed, top):
+    file.write("\n")
+    file.write("# Centralities \n")  
+    file.write("\n")
+    file.write("### " + name + " centrality: \n") 
+    
+    centrality = np.empty((10))
+    for i in range(10):
+        centrality[i] = computed[top[i]]
+    top_index = np.argmax(centrality)
+    for i in range(10):
+        if i != top_index:
+            file.write("* " + str(top[i]) + ": " + str(centrality[i]) + "\n\n")
+        else:
+            file.write("* **" + str(top[i]) + ": " + str(centrality[i]) + "**\n\n")
 
 G=nx.read_pajek("USAir97.net")
 
@@ -11,23 +29,23 @@ with open("slides.md", "w") as file:
     file.write("% Zuzana Drázdová & Zuzana Šimečková \n")
     file.write("% January 7 2020 \n")
     file.write("# General information \n")
-    file.write("US Air 97 Network has **{0}** nodes and **{1}** edges.\n".format(len(G.nodes),len(G.edges)))
-    file.write("Nodes represent Airports in the United States and edges represent routes between these airtports. Each edge has weights with indicated how many flights are on given route")
+    file.write("US Air 97 Network has **{0}** nodes and **{1}** edges.\n\n".format(len(G.nodes),len(G.edges)))
+    file.write("Nodes represent Airports in the United States and edges represent routes between these airtports.\n\n")
+    file.write("Each edge has weights with indicated how many flights are on given route.\n\n")
 
 
 
     sorted_d = sorted([(value, key) for (key,value) in G1.degree], reverse=True)
 
-    file.write("\n")
+    file.write("\n\n")
     file.write("# Top US airports \n")
     
-    file.write("**Top ten airports with most flights:** \n\n")
+    file.write("**Top ten airports with flights to/from most other airports:** \n\n")
     for i in range(10):
         file.write("* " + str(sorted_d[i][1]) + "\n\n")
-        #file.write(sorted_d[:10])
-    Gselection = G
+    Gselection = cp.deepcopy(G)
     
-    #print(G.nodes['San Francisco Intl'])
+
 
     remove = sorted_d[10:]
     nodes_to_remove = []
@@ -36,20 +54,14 @@ with open("slides.md", "w") as file:
         nodes_to_remove.append(node)
         Gselection.remove_node(node)
 
-    pos = nx.circular_layout(Gselection)
 
-
-
-    #print(Gselection.nodes['San Francisco Intl'])
+    plt.figure(figsize=(8,3.4))
     
     positions = {}
     for node in Gselection.nodes:
-        print(Gselection.nodes[node])
         data = Gselection.nodes[node]
         posi = (data['x'],-data['y'])
         positions.setdefault(node, posi)
-    
-    print(positions)
     
     nx.draw_networkx(Gselection, pos=positions, with_labels=True, node_size=100, edge_color="#ff000000")
     
@@ -58,15 +70,40 @@ with open("slides.md", "w") as file:
         edge = [(u,v,d)]
         nx.draw_networkx_edges(Gselection, pos=positions, edgelist=edge, equidistant=True, alpha=0.5, width=w*10)
     
-    plt.savefig('most_flights.svg')
+    plt.savefig('most_other_airports.svg')
     file.write("\n")
     file.write("# Top US airports \n")
-    file.write("![](most_flights.png)")
+    file.write("![Airports with flights to/from most other airports - map](top.png)")
     
     
-    
-    
+    power_dict = {}
 
-#print(G.edges['Minot Intl', 'Stapleton Intl',0])
+    for node in G.nodes:
+        value = 0
+        for edge in G.edges(data=True):
+            (u,v,d) = edge
+            w = d['weight']
+            if node == edge[0] or node == edge[1]:
+                value += w
+        power_dict.setdefault(value, node)
+        
+    sorted_p = sorted(power_dict)[-10:]   
+    sorted_p = reversed(sorted_p)
 
+    file.write("\n\n")
+    file.write("# Top US airports \n")
+    file.write("**Top ten airports with most flights:** \n\n")
+    
+    top = []
+    for i in sorted_p:
+        file.write("* " + str(power_dict[i]) + "\n\n")
+        top.append(power_dict[i])
 
+    G_und = G.to_undirected(reciprocal=False, as_view=False)
+    
+   
+    compute_centrality("In degree", nx.in_degree_centrality(G) ,top)  
+    compute_centrality("Out degree", nx.out_degree_centrality(G) ,top)        
+    compute_centrality("Degree", nx.degree_centrality(G_und) ,top)
+    compute_centrality("Closeness", nx.closeness_centrality(G_und) ,top)
+    
--- slides.md
+++ slides.md
@@ -3,9 +3,15 @@
 % January 7 2020 
 # General information 
 US Air 97 Network has **332** nodes and **2126** edges.
-Nodes represent Airports in the United States and edges represent routes between these airtports. Each edge has weights with indicated how many flights are on given route
+
+Nodes represent Airports in the United States and edges represent routes between these airtports.
+
+Each edge has weights with indicated how many flights are on given route.
+
+
+
 # Top US airports 
-**Top ten airports with most flights:** 
+**Top ten airports with flights to/from most other airports:** 
 
 * Chicago O'hare Intl
 
@@ -29,4 +35,124 @@ Nodes represent Airports in the United States and edges represent routes between
 
 
 # Top US airports 
-![](most_flights.png)
+![Airports with flights to/from most other airports - map](top.png)
+
+# Top US airports 
+**Top ten airports with most flights:** 
+
+* Chicago O'hare Intl
+
+* Dallas/Fort Worth Intl
+
+* Los Angeles Intl
+
+* Minneapolis-St Paul Intl/Wold-
+
+* The William B Hartsfield Atlan
+
+* Seattle-Tacoma Intl
+
+* Stapleton Intl
+
+* San Francisco Intl
+
+* Lambert-St Louis Intl
+
+* Newark Intl
+
+
+# Centralities 
+
+### In degree centrality: 
+* Chicago O'hare Intl: 0.12084592145015105
+
+* **Dallas/Fort Worth Intl: 0.2326283987915408**
+
+* Los Angeles Intl: 0.1268882175226586
+
+* Minneapolis-St Paul Intl/Wold-: 0.04229607250755287
+
+* The William B Hartsfield Atlan: 0.1933534743202417
+
+* Seattle-Tacoma Intl: 0.02416918429003021
+
+* Stapleton Intl: 0.11178247734138973
+
+* San Francisco Intl: 0.11178247734138973
+
+* Lambert-St Louis Intl: 0.13595166163141995
+
+* Newark Intl: 0.060422960725075525
+
+
+# Centralities 
+
+### Out degree centrality: 
+* **Chicago O'hare Intl: 0.2990936555891239**
+
+* Dallas/Fort Worth Intl: 0.12386706948640483
+
+* Los Angeles Intl: 0.0513595166163142
+
+* Minneapolis-St Paul Intl/Wold-: 0.1933534743202417
+
+* The William B Hartsfield Atlan: 0.11178247734138973
+
+* Seattle-Tacoma Intl: 0.14803625377643503
+
+* Stapleton Intl: 0.14501510574018128
+
+* San Francisco Intl: 0.09365558912386707
+
+* Lambert-St Louis Intl: 0.14803625377643503
+
+* Newark Intl: 0.1419939577039275
+
+
+# Centralities 
+
+### Degree centrality: 
+* **Chicago O'hare Intl: 0.4199395770392749**
+
+* Dallas/Fort Worth Intl: 0.3564954682779456
+
+* Los Angeles Intl: 0.1782477341389728
+
+* Minneapolis-St Paul Intl/Wold-: 0.23564954682779457
+
+* The William B Hartsfield Atlan: 0.30513595166163143
+
+* Seattle-Tacoma Intl: 0.17220543806646527
+
+* Stapleton Intl: 0.256797583081571
+
+* San Francisco Intl: 0.2054380664652568
+
+* Lambert-St Louis Intl: 0.283987915407855
+
+* Newark Intl: 0.20241691842900303
+
+
+# Centralities 
+
+### Closeness centrality: 
+* **Chicago O'hare Intl: 0.6073394495412844**
+
+* Dallas/Fort Worth Intl: 0.5544388609715243
+
+* Los Angeles Intl: 0.5245641838351822
+
+* Minneapolis-St Paul Intl/Wold-: 0.5399673735725938
+
+* The William B Hartsfield Atlan: 0.5355987055016181
+
+* Seattle-Tacoma Intl: 0.5262321144674086
+
+* Stapleton Intl: 0.5245641838351822
+
+* San Francisco Intl: 0.533011272141707
+
+* Lambert-St Louis Intl: 0.5287539936102237
+
+* Newark Intl: 0.4932935916542474
+
